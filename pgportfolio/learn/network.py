f3073baf39f4993ed3d2794b2ec99de7b621ffa3
Justin Kasin <s1545572@sms.ed.ac.uk>
2020-02-08 13:22:52 +0000

    fix API mismatch in tensorflow.zeros_initializer: see stackoverflow.com/a/42138402/12546478 for details

--- pgportfolio/learn/network.py
+++ pgportfolio/learn/network.py
@@ -113,7 +113,7 @@ class CNN(NeuralNetWork):
                 network = network[:, :, 0, 0]
                 #btc_bias = tf.zeros((self.input_num, 1))
                 btc_bias = tf.get_variable("btc_bias", [1, 1], dtype=tf.float32,
-                                       initializer=tf.zeros_initializer)
+                                       initializer=tf.zeros_initializer())
                 # self.add_layer_to_dict(layer["type"], network, weights=False)
                 btc_bias = tf.tile(btc_bias, [self.input_num, 1])
                 network = tf.concat([btc_bias, network], 1)
@@ -121,7 +121,6 @@ class CNN(NeuralNetWork):
                 self.add_layer_to_dict('voting', network, weights=False)
                 network = tflearn.layers.core.activation(network, activation="softmax")
                 self.add_layer_to_dict('softmax_layer', network, weights=False)
-
             elif layer["type"] == "EIIE_LSTM" or\
                             layer["type"] == "EIIE_RNN":
                 network = tf.transpose(network, [0, 2, 3, 1])
